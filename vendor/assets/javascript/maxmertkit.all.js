c47321caf9e237ffe7544d57a07f9f1529c69e28
Viktor Tomilin <caballerosolar@gmail.com>
2013-01-16 22:39:15 +0200

    Added js file which includes all maxmertkit scripts

---
+++ vendor/assets/javascript/maxmertkit.all.js
@@ -0,0 +1,14 @@
+//= require maxmertkit
+//= require maxmertkit.notify
+//= require maxmertkit.button
+//= require maxmertkit.affix
+//= require maxmertkit.carousel
+//= require maxmertkit.modal
+//= require maxmertkit.popup
+//= require maxmertkit.scrollspy
+//= require maxmertkit.tabs
+
+//= require libs/easing
+//= require libs/html5shiv
+//= require libs/imagesLoaded
+//= require libs/modernizr
